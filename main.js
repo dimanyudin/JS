cd7f58520a96796c7297f3f642971f1e03d120ba
Dima <example@gmail.com>
2022-04-25 14:36:37 +0700

    уроки по методам и практика

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
 "use strict"
 
-const numberOfFilms = prompt("сколько фильмов?","");
+let numberOfFilms;
+
+function start() {
+	numberOfFilms = +prompt("сколько фильмов?","");
+	/* вариант с for */
+/* 	for(let i = 0; i < 1; i++){
+		if(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+			i--;
+			numberOfFilms = +prompt("сколько фильмов?","");
+		}
+	} */
+	/* вариант с while */
+	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+		numberOfFilms = +prompt("сколько фильмов?","");
+	}
+}
+start();
 
 
 const persanalMovieDb = {
@@ -11,8 +27,10 @@ const persanalMovieDb = {
 	privat: false
 }
 
-/* for */
- for (let i = 0; i < 2; i++) {
+
+function firsFilms() {
+	/* for */
+	for (let i = 0; i < 2; i++) {
 	const a = prompt("последний фильм",""),
 				 b = prompt("оценка","");
 
@@ -56,7 +74,11 @@ do{
 	}
 }while(i < 2);
  */
-if(persanalMovieDb.count <= 10){
+}
+firsFilms();
+ 
+function filmsCount() {
+	if(persanalMovieDb.count <= 10){
 	console.log('Просмотрено довольно мало фильмо');
 }else if(persanalMovieDb.count > 10 && persanalMovieDb.count <= 30){
 	console.log('Вы классический зритель');
@@ -67,4 +89,21 @@ if(persanalMovieDb.count <= 10){
 	console.log('error');
 	
 }
-console.log(persanalMovieDb);
+}
+filmsCount();
+function filmsCategory() {
+	for(let i = 1; i <=3; i++){
+		persanalMovieDb.genres[i - 1] = prompt(` любимый жанр № ${i}`)
+	}
+}
+filmsCategory();
+function privat(hidden) {
+	if (!hidden) {
+		console.log(persanalMovieDb);
+	}
+}
+privat(persanalMovieDb.privat);
+
+
+
+
